39b9f295e6261ee064508219af9af548cccf8d99
Shers_um49 <Shers_um49@student.itstep.org>
2026-03-22 23:34:47 +0200

    Added React Project with name `FamilyClub.React` so now we can develop our frontend separately and it will work together. In `FamilyClub.WebApi` project added WebApi empty Controller with name `Home` to test how projects are working together. So far connection established. Thank's everyone for attention.
    Додано React-проект з назвою `FamilyClub.React`, тепер ми можемо розробляти фронтенд окремо, і все працюватиме разом. У проєкті `FamilyClub.WebApi` додано порожній контролер типу WebApi з назвою `Home` для перевірки того, як проєкти працюють разом. Наразі з'єднання встановлено. Дякую всім за увагу.

--- FamilyClub.WebAPI/Program.cs
+++ FamilyClub.WebAPI/Program.cs
@@ -1,22 +1,83 @@
+using FamilyClub.WebAPI.Data;
+using FamilyClubLibrary;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Allowing our requests from React
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReact",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:5173") // React URL
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
 
-//builder.Services.AddControllers();
-builder.Services.AddControllersWithViews();
+
+
+// MVC + Views
+// Add services to the container.
+builder.Services.AddControllers();
+//builder.Services.AddControllersWithViews();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
 builder.Services.AddEndpointsApiExplorer();
 
+// Connection string
+string connStr = builder.Configuration.GetConnectionString("FamilyClubContext")
+    ?? throw new InvalidOperationException("Connection string 'FamilyClubContext' not found!");
+
+// DB CONTEXT
+builder.Services.AddDbContext<FamilyClubContext>(options => {
+    options.UseSqlServer(connStr);
+});
+
+// Identity
+builder.Services.AddIdentity<ClubMember, IdentityRole>()
+    .AddEntityFrameworkStores<FamilyClubContext>()
+    .AddDefaultTokenProviders();
+
+// Customize Identity cookie
+//builder.Services.ConfigureApplicationCookie(
+//    options => {
+//        options.LoginPath = "/Account/Login";
+//        options.AccessDeniedPath = "/Account/AccessDenied";
+//    });
+
+// Adding AutoMapper
+builder.Services.AddAutoMapper(cfg =>
+{
+    // Here will be added `Profiles` like on example below:
+    //cfg.AddProfile(new ProductProfile());
+}
+);
+
+builder.Services.AddSwaggerGen();
+
+
 var app = builder.Build();
+
+app.UseCors("AllowReact"); // Allowing to use React
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
+app.UseStaticFiles(); // Serve static files from wwwroot
+
+
+app.UseDefaultFiles(); // Serve default files like index.html
 
 app.UseAuthentication();
 app.UseAuthorization();
@@ -24,7 +85,9 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+//app.MapControllerRoute(
+//    name: "default",
+//    pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.MapGet("/", () => "Hello world!");
 
 app.Run();
