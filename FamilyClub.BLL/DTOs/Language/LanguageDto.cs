904471138decf55eb64426ede513e53af771804b
Shers_um49 <Shers_um49@student.itstep.org>
2026-04-01 00:09:52 +0300

    Added DTO, Services and Interfaces for `Language` entity.

---
+++ FamilyClub.BLL/DTOs/Language/LanguageDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FamilyClub.BLL.DTOs.Language
+{
+    public class LanguageDto
+    {
+        public int Id { get; set; }
+        public string LanguageName { get; set; } = default!;
+    }
+}
---
+++ FamilyClub.BLL/Interfaces/ILanguageService.cs
@@ -0,0 +1,12 @@
+﻿using FamilyClub.BLL.DTOs.Language;
+
+namespace FamilyClub.BLL.Interfaces;
+
+public interface ILanguageService
+{
+    Task<IEnumerable<LanguageDto>> GetAllAsync(CancellationToken cancellationToken = default);
+    Task<LanguageDto?> GetByIdAsync(int id, CancellationToken cancellationToken = default);
+    Task<LanguageDto> CreateAsync(LanguageDto dto, CancellationToken cancellationToken = default);
+    Task<bool> UpdateAsync(int id, LanguageDto dto, CancellationToken cancellationToken = default);
+    Task<bool> DeleteAsync(int id, CancellationToken cancellationToken = default);
+}
