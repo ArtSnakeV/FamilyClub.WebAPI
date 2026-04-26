232069ca102b6ab81925d7a7d02a2fc9ee3787d4
Шепель Ярослав Валерійович <160532292+yaroslavshepel@users.noreply.github.com>
2026-04-26 23:25:18 +0300

    повернено перевірку підключення до бекенду. статус - в консолі браузера

--- FamilyClub.React/famiy.club.react/app/page.tsx
+++ FamilyClub.React/famiy.club.react/app/page.tsx
@@ -33,9 +33,27 @@ import Features from "@/app/main_page/Features";
 import Banner from "@/app/main_page/Banner";
 import AboutSection from "@/app/main_page/AboutSection";
 
-export default function Home() {
-  return (
-      <main className="bg-[#f5f2ee]">
+export default async function Home() {
+    let message = "Loading...";
+    try {
+        const response = await fetch('https://localhost:7069/api/Home', {
+                cache: 'no-store' // This ensures we get fresh data every time
+        });
+
+        if (response.ok) {
+            message = await response.text(); // We use .text() because the API returns a string
+            console.log(message);
+        } else {
+            message = "Error: Backend reached but returned " + response.status;
+            console.error(message);
+        }
+    }
+    catch (error) {
+        console.log("REAL ERROR:", error);
+        message = "Connection Failed: Is the Backend running?";
+    }
+    return (
+        <main className="bg-[#f5f2ee]">
             <Hero />
 
             <BookSection title="Рекомендації для тебе" />
@@ -50,17 +68,17 @@ export default function Home() {
 
             <Features />
 
-              <BookSection title="Роман" />
-              <BookSection title="Трилери" />
-              <BookSection title="Наукові" />
-              <BookSection title="Фантастика" />
+            <BookSection title="Роман" />
+            <BookSection title="Трилери" />
+            <BookSection title="Наукові" />
+            <BookSection title="Фантастика" />
 
-              <Banner />
+            <Banner />
 
-              <BookSection title="Хіти продажу" />
-              <BookSection title="Новинки" />
-              <BookSection title="Комплекти" />
-              <BookSection title="Анонси" />
-      </main>
-  );
+            <BookSection title="Хіти продажу" />
+            <BookSection title="Новинки" />
+            <BookSection title="Комплекти" />
+            <BookSection title="Анонси" />
+        </main>
+    );
 }
