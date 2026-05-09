38c8e708a58facc9571d0d46d0a3f49a9145d8e6
KaterynaRe <katyareva88@gmail.com>
2026-05-10 00:54:44 +0300

    update add product right

--- FamilyClub.React/famiy.club.react/app/layout/header/dropdownlist/DropDownList.tsx
+++ FamilyClub.React/famiy.club.react/app/layout/header/dropdownlist/DropDownList.tsx
@@ -1,20 +1,22 @@
+"use client";
+
 import DropDownAuthors from "./DropDownAuthors";
 import DropDownCategories from "./DropDownCategories";
-import DropDownLanguage from "./DropDownLanguage";
 import DropDownFormat from "./DropDownFormat";
+import DropDownLanguage from "./DropDownLanguage";
 import DropDownPrice from "./DropDownPrice";
 import DropDownYearOfPublication from "./DropDownYearOfPublication";
 
-
 export default function DropDownList() {
-    return (
-        <>
-           <DropDownCategories />
-           <DropDownAuthors />
-           <DropDownLanguage />
-           <DropDownFormat />
-           <DropDownPrice />
-           <DropDownYearOfPublication />
-        </>
-    )
+
+  return (
+    <>
+      <DropDownCategories />
+      <DropDownAuthors />
+      <DropDownLanguage />
+      <DropDownFormat />
+      <DropDownPrice />
+      <DropDownYearOfPublication />
+    </>
+  );
 }
