fa2e9d2a8a47d44cf41c9185d6e2b28d744b5693
KaterynaRe <katyareva88@gmail.com>
2026-05-18 22:28:14 +0300

    update header px

--- FamilyClub.React/famiy.club.react/app/layout/header/UaCircle.tsx
+++ FamilyClub.React/famiy.club.react/app/layout/header/UaCircle.tsx
@@ -1,31 +1,59 @@
 import Link from "next/link";
 import Image from "next/image";
 
-
 export default function UaCircle() {
-
   return (
     <div className="flex items-center justify-center gap-3">
-      <div className="flex items-center justify-center w-[50px] h-[50px] ">
-        <Image
-          src="/images/Group11.png"
-          alt="group11"
-          width={36}
-          height={36}
-          className="object-contain"
-          priority
-        />
+      <div className="group flex items-center justify-center">
+        <div
+          className="
+          w-[50px]
+          h-[50px]
+          flex
+          items-center
+          justify-center
+          rounded-full
+          transition-all
+          duration-300
+          group-hover:bg-[var-(--color-white)]
+          group-hover:shadow-[0px_0px_15px_0px_#242424CC]
+        "
+        >
+          <Image
+            src="/images/Group11.png"
+            alt="group11"
+            width={36}
+            height={36}
+            className="object-contain"
+            priority
+          />
+        </div>
       </div>
-      <div className="flex items-center justify-center w-[50] h-[50] ">
-        <Image
-          src="/images/header/Group19.png"
-          alt="group19"
-          width={36}
-          height={36}
-          className="object-contain"
-          priority
-        />
+      <div className="group flex items-center justify-center">
+        <div
+          className="
+          w-[50px]
+          h-[50px]
+          flex
+          items-center
+          justify-center
+          rounded-full
+          transition-all
+          duration-300
+          group-hover:bg-[var-(--color-white)]
+          group-hover:shadow-[0px_0px_15px_0px_#242424CC]
+        "
+        >
+          <Image
+            src="/images/header/Group19.png"
+            alt="group19"
+            width={36}
+            height={36}
+            className="object-contain"
+            priority
+          />
+        </div>
       </div>
     </div>
-  )
+  );
 }
