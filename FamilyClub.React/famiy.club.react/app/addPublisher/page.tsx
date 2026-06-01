5f433e2063053bd57566fe49b0dc1fc059e52319
KaterynaRe <katyareva88@gmail.com>
2026-06-01 17:25:09 +0300

    upate author/product/category/publisher

--- FamilyClub.React/famiy.club.react/app/addPublisher/page.tsx
+++ FamilyClub.React/famiy.club.react/app/addPublisher/page.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 import { BasicInfoSection } from "./sections/BasicInfoSection";
 import { useSubmitPublisher } from "./hooks/useSubmitPublisher";
 import { usePublisherForm } from "./hooks/usePublisherForm";
+import ButtonReturn from "./ButtonReturn";
 
 export default function AddPublisherPage() {
   const router = useRouter();
@@ -11,17 +12,26 @@ export default function AddPublisherPage() {
   const { handleSubmit, loading } = useSubmitPublisher({ form, router });
 
   return (
-    <div className="w-full min-h-screen flex flex-col">
+    <div
+      className="w-full min-h-screen flex flex-col "
+      style={{
+        backgroundImage: "url('/images/addProducts/Rectangle 326.png')",
+        backgroundSize: "cover",
+      }}
+    >
       <div
-        className="relative w-[1200px] pb-[60px] -mt-[68px] mx-auto bg-no-repeat"
+        className="relative w-[900px] ml-[16.7vw] -mt-[68px] mx-auto bg-no-repeat min-h-screen"
         style={{
           backgroundImage: "url('/images/addProducts/Rectangle 312.svg')",
           backgroundSize: "cover",
           backgroundPosition: "top",
         }}
       >
-        <div className="flex flex-col items-center mt-[120px]">
-          <h1 className="text-[var(--color-black)] w-[800px] font-['Roboto_Mono'] font-bold text-[64px] leading-[150%] tracking-[-0.011em] text-center">
+        <div className="flex z-40 relative top-[130px] ml-[64px]">
+          <ButtonReturn />
+        </div>
+        <div className="flex flex-col items-center mt-[80px]">
+          <h1 className="text-[var(--color-black)] w-[800px] font-['Roboto_Mono'] font-bold text-[46px] leading-[150%] tracking-[-0.011em] text-center">
             Додати видавництво
           </h1>
         </div>
@@ -32,8 +42,8 @@ export default function AddPublisherPage() {
             handleSubmit();
           }}
         >
-          <div className="w-full flex mt-[48px] gap-[4vw] justify-center">
-            <div className="w-[645px] flex flex-col">
+          <div className="w-full flex justify-center ml-2 items-center">
+            <div className="w-[485px] relative flex flex-col">
               <BasicInfoSection
                 form={form}
                 setField={setField}
